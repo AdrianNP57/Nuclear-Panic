813cbe64242d57f6c541ecc4c9872a6d9b748b23
QeaZz <jespjan@student.chalmers.se>
2019-10-01 18:49:03 +0200

    Main Menu
    -Merged in the base main menu I created with the rest.
    -The main menu canvas is now a prefab.
    -Made a new "MainMenuScene" scene.
    -Added an EventSystem for the new scene.
    -Added TextMeshPro, since its a part of the menu.

---
+++ Assets/Scripts/UI/Menus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Quit()
+    {
+        Debug.Log("Game Ended");
+        Application.Quit();
+    }
+
+}
