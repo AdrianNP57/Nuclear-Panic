e66f03e3b42fc4132cbabdeecec737b0846e7844
AsharImtiaz <ashar.imtiaz1992@gmail.com>
2019-10-09 11:46:56 +0200

    ParallaxEffect... sort of added
    Parallax Effect with 4 backgrounds added with varying speeds

---
+++ Assets/ParallaxEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private float length, startPos;
+    public GameObject cam;
+    public float parallaxEffect;
+    private float dist;
+    private float temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        temp = (cam.transform.position.x * (1.0f - parallaxEffect));
+        dist = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+        if(temp > startPos + length)
+        {
+            startPos += length;
+        }
+        else if(temp < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
