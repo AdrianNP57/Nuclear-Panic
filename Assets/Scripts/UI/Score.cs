b38891ff2badfc209191e27a2739315e731db187
QeaZz <jespjan@student.chalmers.se>
2019-10-13 20:41:05 +0200

    Score
    -Added scoreText
    -Added script for the scoreText behavior

---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public TextMeshProUGUI scoreText;
+
+    void Update()
+    {
+        scoreText.text = player.position.x.ToString("0"); //sets scoreText to an integer depending on the x position of the player
+    }
+}
