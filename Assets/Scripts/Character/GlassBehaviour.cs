0d23770d93e9150f8164313593c566512a38f0b9
AsharImtiaz <ashar.imtiaz1992@gmail.com>
2019-10-14 12:57:07 +0200

    Reduced the lightening radius and now you can see the background in the light as well

--- Assets/Scripts/Character/GlassBehaviour.cs
+++ Assets/Scripts/Character/GlassBehaviour.cs
@@ -6,6 +6,16 @@ using UnityEngine.Tilemaps;
 public class GlassBehaviour : MonoBehaviour
 {
     public GameObject tileMap;
+    public GameObject firstBackground;
+    public GameObject firstBackgroundpartOne;
+    public GameObject secondBackground;
+    public GameObject secondBackgroundpartOne;
+    public GameObject thirdBackground;
+    public GameObject thirdBackgroundpartOne;
+    public GameObject thirdBackgroundpartTwo;
+    public GameObject fourthBackground;
+    public GameObject fourthBackgroundpartOne;
+    public GameObject fourthBackgroundpartTwo;
     public Material darkMaterial;
     public GameObject haloLight;
     private GameObject backgroundPrefab;
@@ -16,6 +26,10 @@ public class GlassBehaviour : MonoBehaviour
     public Sprite glassesOffSprite;
 
     Material defaultMaterial;
+    Material defaultMaterialBackground1;
+    Material defaultMaterialBackground2;
+    Material defaultMaterialBackground3;
+    Material defaultMaterialBackground4;
 
     bool glassesOn;
     Camera changeColor;
@@ -25,8 +39,12 @@ public class GlassBehaviour : MonoBehaviour
         glassesOn = true;
         changeColor = Camera.main.GetComponent<Camera>();
         defaultMaterial = GameObject.Find("Tilemap").GetComponent<TilemapRenderer>().material;
+        defaultMaterialBackground1 = GameObject.Find("4").GetComponent<SpriteRenderer>().material;
+        defaultMaterialBackground2 = GameObject.Find("3").GetComponent<SpriteRenderer>().material;
+        defaultMaterialBackground3 = GameObject.Find("2").GetComponent<SpriteRenderer>().material;
+        defaultMaterialBackground4 = GameObject.Find("1").GetComponent<SpriteRenderer>().material;
         backgroundPrefab = GameObject.Find("Background");
-        backgroundPrefab.SetActive(false);
+        //backgroundPrefab.SetActive(false);
     }
 
     // Update is called once per frame
@@ -37,22 +55,36 @@ public class GlassBehaviour : MonoBehaviour
         {
             Debug.Log("Glasses on");
             glassesOn = true;
-            backgroundPrefab.SetActive(false);
+            //backgroundPrefab.SetActive(false);
         }
         else if (Input.GetButtonDown("Glasses") && glassesOn)
         {
             Debug.Log("Glasses off");
             glassesOn = false;
-            backgroundPrefab.SetActive(true);
+            //backgroundPrefab.SetActive(true);
         }
 
         Material targetMaterial = glassesOn? darkMaterial : defaultMaterial;
         Color backgroundColor = glassesOn? new Color(0.0f, 0.0f, 0.0f) : new Color(0.2f, 0.3f, 0.5f);
 
+        Material targetMaterial1 = glassesOn ? darkMaterial : defaultMaterial;
+        //Color backgroundColor1 = glassesOn ? new Color(0.0f, 0.0f, 0.0f) : new Color(0.2f, 0.3f, 0.5f);
+
         glassesRenderer.sprite = glassesOn ? glassesOnSprite : glassesOffSprite;
         haloLight.SetActive(glassesOn);
         changeColor.backgroundColor = backgroundColor;
         tileMap.GetComponent<TilemapRenderer>().material = targetMaterial;
+        firstBackground.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        firstBackgroundpartOne.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        secondBackground.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        secondBackgroundpartOne.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        thirdBackground.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        thirdBackgroundpartOne.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        thirdBackgroundpartTwo.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        fourthBackground.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        fourthBackgroundpartOne.GetComponent<SpriteRenderer>().material = targetMaterial1;
+        fourthBackgroundpartTwo.GetComponent<SpriteRenderer>().material = targetMaterial1;
+
         foreach (GameObject level in levelPool.GetComponent<LevelPoolManager>().levels)
         {
 
