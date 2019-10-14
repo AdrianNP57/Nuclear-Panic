2f0523b5b4c76c3dba1e5635792b2761ada72747
QeaZz <jespjan@student.chalmers.se>
2019-10-14 23:52:45 +0200

    Pre-UserTest
    -Added 2 new level designs. (23 & 24)
    -Fixed raditation bug when particles overlap.
    -Added digit with 2 decemals for the radiation bar.
    -Added first-draft-purposal of how to modify the score amount per unit.

--- Assets/Scripts/Radiation/RadiationCollision.cs
+++ Assets/Scripts/Radiation/RadiationCollision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RadiationCollision : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D coll) 
+    private void OnTriggerStay2D(Collider2D coll) 
     {
         if(coll.gameObject.tag == "LowRadiation") //Low Collision
         {
