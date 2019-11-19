594c0e441a15adb52ef083b85ac0e30184a1f3f9
Samuel <samuelsoutullosobral@gmail.com>
2019-11-19 17:14:24 +0100

    Added script to have different images depending on platform

---
+++ Assets/Scripts/UI/ImageForPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageForPlatform : MonoBehaviour
+{
+    // Properties
+    public Sprite defaultSprite;
+    public PlatformSprite[] platformSprites;
+
+    void Awake()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = defaultSprite;
+
+        foreach(PlatformSprite platformSprite in platformSprites)
+        {
+            if(Application.platform == platformSprite.platform)
+            {
+                renderer.sprite = platformSprite.sprite;
+            }
+        }
+    }
+}
+
+[Serializable]
+public struct PlatformSprite
+{
+    public RuntimePlatform platform;
+    public Sprite sprite;
+}
